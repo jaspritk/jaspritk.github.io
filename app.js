3b0675b6bb15f297cad58664ed25d79e6283b7bc
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 <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>
2022-06-27 15:22:12 +0530

    changes in styling

--- app.js
+++ app.js
@@ -4,54 +4,71 @@ var svg = document.querySelectorAll('svg');
 var img = document.querySelectorAll('img');
 var h3 = document.querySelectorAll('h3');
 
-document.addEventListener('mousemove', function(e){
+document.addEventListener('mousemove', function (e) {
   var x = e.clientX;
   var y = e.clientY;
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
 });
 
-document.addEventListener('mousemove', function(e){
+document.addEventListener('mousemove', function (e) {
   var x = e.clientX;
   var y = e.clientY;
   cursorinner.style.left = x + 'px';
   cursorinner.style.top = y + 'px';
 });
 
-document.addEventListener('mousedown', function(){
+document.addEventListener('mousedown', function () {
   cursor.classList.add('click');
   cursorinner.classList.add('cursorinnerhover')
 });
 
-document.addEventListener('mouseup', function(){
+document.addEventListener('mouseup', function () {
   cursor.classList.remove('click')
   cursorinner.classList.remove('cursorinnerhover')
 });
 
 
 svg.forEach(item => {
-    item.addEventListener('mouseover', () => {
+  item.addEventListener('mouseover', () => {
     cursor.classList.add('hover');
-    });
-    item.addEventListener('mouseleave', () => {
+  });
+  item.addEventListener('mouseleave', () => {
     cursor.classList.remove('hover');
-    });
+  });
 })
 
 img.forEach(item => {
   item.addEventListener('mouseover', () => {
-  cursor.classList.add('hover');
+    cursor.classList.add('hover');
   });
   item.addEventListener('mouseleave', () => {
-  cursor.classList.remove('hover');
+    cursor.classList.remove('hover');
   });
 })
 
 h3.forEach(item => {
   item.addEventListener('mouseover', () => {
-  cursor.classList.add('hover');
+    cursor.classList.add('hover');
   });
   item.addEventListener('mouseleave', () => {
-  cursor.classList.remove('hover');
+    cursor.classList.remove('hover');
   });
 })
 
+// Copyright year
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
+
+
+// Progress scrollbar
+function progressBarScroll() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+    height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+    scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+window.onscroll = function () {
+  progressBarScroll();
+};
