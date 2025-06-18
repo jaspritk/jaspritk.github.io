28e17b72c492c49c825ef6c49463858f90d51fe3
jaspritk <jaspritkaur2920@gmail.com>
2025-06-18 09:39:08 -0400

    new case studies added and resume updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WorkComponent } from './components/work/work.component';
-import { ArtsComponent } from './components/arts/arts.component';
+// import { ArtsComponent } from './components/arts/arts.component';
 import { AboutComponent } from './components/about/about.component';
-import { BusinessComponent } from './components/business/business.component';
+// import { BusinessComponent } from './components/business/business.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/work', pathMatch: 'full' },
   // { path: '', component: WorkComponent },
   { path: 'work', component: WorkComponent },
-  { path: 'business', component: BusinessComponent },
-  { path: 'arts', component: ArtsComponent },
+  // { path: 'business', component: BusinessComponent },
+  // { path: 'arts', component: ArtsComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '/work', pathMatch: 'full' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,36 +6,42 @@ import { AppComponent } from './app.component';
 
 import { DataAnalysisComponent } from './components/data-analysis/data-analysis.component';
 import { CaseStudiesComponent } from './components/case-studies/case-studies.component';
-import { WebinarsComponent } from './components/webinars/webinars.component';
-import { FinalYearComponent } from './components/final-year/final-year.component';
-import { InternshipComponent } from './components/internship/internship.component';
-import { NpmPackagesComponent } from './components/npm-packages/npm-packages.component';
-import { MeanStackComponent } from './components/mean-stack/mean-stack.component';
-import { FreelancingComponent } from './components/freelancing/freelancing.component';
-import { ArtsComponent } from './components/arts/arts.component';
+// import { WebinarsComponent } from './components/webinars/webinars.component';
+// import { FinalYearComponent } from './components/final-year/final-year.component';
+// import { InternshipComponent } from './components/internship/internship.component';
+// import { NpmPackagesComponent } from './components/npm-packages/npm-packages.component';
+// import { MeanStackComponent } from './components/mean-stack/mean-stack.component';
+// import { FreelancingComponent } from './components/freelancing/freelancing.component';
+// import { ArtsComponent } from './components/arts/arts.component';
 import { WorkComponent } from './components/work/work.component';
 import { AboutComponent } from './components/about/about.component';
-import { SeoComponent } from './components/seo/seo.component';
-import { BusinessComponent } from './components/business/business.component';
-import { BadgesComponent } from './components/badges/badges.component';
+// import { SeoComponent } from './components/seo/seo.component';
+// import { BusinessComponent } from './components/business/business.component';
+// import { BadgesComponent } from './components/badges/badges.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SkillsComponent } from './components/skills/skills.component';
+// import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataAnalysisComponent,
     CaseStudiesComponent,
-    WebinarsComponent,
-    FinalYearComponent,
-    InternshipComponent,
-    NpmPackagesComponent,
-    MeanStackComponent,
-    FreelancingComponent,
-    ArtsComponent,
+    // WebinarsComponent,
+    // FinalYearComponent,
+    // InternshipComponent,
+    // NpmPackagesComponent,
+    // MeanStackComponent,
+    // FreelancingComponent,
+    // ArtsComponent,
     WorkComponent,
     AboutComponent,
-    SeoComponent,
-    BusinessComponent,
-    BadgesComponent
+    // SeoComponent,
+    // BusinessComponent,
+    // BadgesComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    // ExperienceComponent
   ],
   imports: [
     BrowserModule,
